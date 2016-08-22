b89d672253b93832f19d96cfabfa07bafb205cd3
Shahid Khan <shahidcaan@gmail.com>
2016-08-22 10:37:16 +0400

    Source code for all the Tasks

--- src/Task5.py
+++ src/Task5.py
@@ -41,12 +41,66 @@ def sortRevAlphaTitles(year):
     yearTitles.sort(reverse=True)
     return yearTitles
 
-def printSortedTitles(year):
-    sortedTitlesList = sortRevAlphaTitles(year)
+"""
+Task 5c
+Write a function printSortedTitles(sortedTitlesList), that takes
+one parameter sorted titles list of type List, loop through it
+using the for loop and display the titles in the list. 
+"""
+def printSortedTitles(sortedTitlesList):
     for title in sortedTitlesList:
         print(title)
+"""
+Task 5d
+Write a function chooseSorting that displays the choice menu to
+the user to choose the sorting type (Part a or Part c) and execute
+the appropriate function. Within the choice the program asks the user
+to enter the year. The function should display the menu as follows:
 
-printSortedTitles("2015")
+Enter 1 to sort Movies Titles from A-Z
+Enter 2 to sort Movies Titles from Z-A 
+Enter 3 to exit the program
+
+"""
+def chooseSorting():
+
+    userChoice = ""
+
+    while (userChoice != "3"):
+
+        userChoice = input("***************************************"\
+                           "\nEnter 1 to sort Movies Titles from A-Z"\
+                           "\nEnter 2 to sort Movies Titles from Z-A "\
+                           "\nEnter 3 to exit the program"\
+                           "\n*************************************"\
+                           "\nEnter your choice here : ")
+        sortedTitlesList = []
+        if userChoice == "1":
+            year = input ("Enter the movies year : ")
+            sortedTitlesList = sortAlphaTitles(year)
+            printSortedTitles(sortedTitlesList)
+
+        elif userChoice == "2":
+            year = input ("Enter the movies year : ")
+            sortedTitlesList = sortRevAlphaTitles(year)
+            printSortedTitles(sortedTitlesList)
+
+        else:
+            print("Program Terminated...")
+   
+
+chooseSorting()
+
+
+
+
+
+
+
+
+
+
+
 
 
 
